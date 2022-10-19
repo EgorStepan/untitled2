cc5b24a262f2743a373a55d2bd3048c16b57b57f
EgorStepan <stepegorr@gmail.com>
2022-10-19 13:57:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int x;
+        x= args[0];
+        switch(x){
+            case x>args[1]{
+                System.out.println("2");
+                    break;
+            }
+            case x<args[1]{
+                System.out.println("1");
+                break;
+            }
+            default{
+                System.out.println(x);
+            }
+        }
+    }
+}
