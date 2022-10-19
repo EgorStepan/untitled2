5875da5624a29523a852bcba2d41fb63cb553352
EgorStepan <stepegorr@gmail.com>
2022-10-19 19:36:51 +0300

    haha

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int x;
+        int x = Integer.parseInt(args[0]);
+
         switch (x){
             case 10:{
                 System.out.println("123");
