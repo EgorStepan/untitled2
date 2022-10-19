be2561a2d64a80a007e71160f5fd5c956caa3d0b
EgorStepan <stepegorr@gmail.com>
2022-10-19 20:45:40 +0300

    haha

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int x = Integer.parseInt(args[0]);
+        Scanner read = new Scanner(System.in);
+        String choise = read.nextLine();
 
-        switch (x){
-            case 10:{
-                System.out.println("123");
-                break;
+        while (!choise.equals("Exit") && !choise.equals("exit") ) {
+            switch (choise) {
+                case "hello": {
+                    System.out.println("123");
+                    break;
+                }
+                case "hi": {
+                    System.out.println("11223");
+                    break;
+                }
+                default: {
+                    System.out.println("12344313");
+                }
             }
-            case 12:{
-                System.out.println("11223");
-                break;
-            }
-            default:{
-                System.out.println("12344313");
-            }
+            choise = read.nextLine();
         }
-
 
 
     }
