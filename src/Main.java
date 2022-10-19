b717ed1aab454e97332943a0f2ece13127b6adf0
EgorStepan <stepegorr@gmail.com>
2022-10-19 19:14:11 +0300

    agd

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int x;
-        x= args[0];
-        switch(x){
-            case x>args[1]{
-                System.out.println("2");
-                    break;
-            }
-            case x<args[1]{
-                System.out.println("1");
+        switch (x){
+            case 10:{
+                System.out.println("123");
                 break;
             }
-            default{
-                System.out.println(x);
+            case 12:{
+                System.out.println("11223");
+                break;
+            }
+            default:{
+                System.out.println("12344313");
             }
         }
-        int sd;
-        sd=0;
+
+
 
     }
 }
